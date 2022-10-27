f75975829749ffab8e98f2a0a3eaf39ec76df1c7
malinjr07 <malinjr07@gmail.com>
2022-10-27 13:59:36 +0600

    typescript configured

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: '.',
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          '@components': './components',
+          '@screens': './screens',
+          '@styles': './styles',
+        },
+      },
+    ],
+  ],
 };
